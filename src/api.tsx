593c8859f769c78cd9976832b542d8a7d973b194
Ylandolsi <yesslandolsi@gmail.com>
2025-03-29 02:45:48 +0100

    update

--- src/api.tsx
+++ src/api.tsx
@@ -1,1 +1,2 @@
-export const APIURL = 'http://localhost:5080/api';
+// export const APIURL = 'http://localhost:5080/api'; // localhost
+export const APIURL = 'http://localhost:5160/api'; // docker container
